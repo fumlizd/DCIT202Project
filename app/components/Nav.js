be09a115bcefc38da48cb805d785058b70da784c
fumlizd <elizabethakuafumdick@gmail.com>
2021-11-17 23:54:17 +0000

    modified nav bottom

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,80 +1,22 @@
-import React, { useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
-import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
-import { Text, TouchableOpacity, View, Image, StyleSheet } from "react-native";
-
-const ProductDetails = ({ route, navigation }) => {
-  const dispatch = useDispatch();
-
-  const { id, retailPrice, title, media, year, brand, gender } = route.params;
-
+const Nav = () => {
   return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: "#fff", position: "relative" }}
-    ></SafeAreaView>
+    <View style={{ flexDirection: "row", flex: 1 }}>
+      <View
+        style={{ flex: 0.5, justifyContent: "center", alignItems: "center" }}
+      >
+        <Feather size={24} color="grey" name="home" />
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Feather size={24} color="grey" name="shopping-bag" />
+      </View>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  productHeader: {
-    flexDirection: "row",
-    width: "100%",
-    padding: 20,
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    flexBasis: "10%",
-  },
-  productGender: {
-    flexDirection: "row",
-    flexBasis: "8%",
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  productGenderText: {
-    backgroundColor: "#dd2",
-    padding: 10,
-    borderRadius: 5,
-    textTransform: capitalize,
-    fontSize: 16,
-  },
-  productImage: {
-    flexBasis: "38%",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    padding: 10,
-  },
-  productName: {
-    flexBasis: "10%",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    textAlign: "center",
-    backgroundColor: "#f0f0f0",
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-  },
-  productSize: {
-    flexBasis: "10%",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    backgroundColor: "#f0f0f0",
-    alignItems: "center",
-  },
-  productSizeContainer: {
-    backgroundColor: "#f0f0f0",
-    padding: 10,
-    backgroundColor: "#f0f0f0",
-    borderRadius: 10,
-  },
-  productPrice: {
-    flexDirection: "row",
-    flex: 1,
-    borderTopRightRadius: 20,
-    overflow: "hidden",
-  },
-});
+const styles = StyleSheet.create({});
 
-export default ProductDetails;
+export default Nav;
